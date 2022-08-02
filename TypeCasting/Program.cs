3ebd674ccb1c8129e9a4087ea2d6eb63804a1c70
Aj007ar <aj007.ar@gmail.com>
2022-08-02 14:17:43 +0530

    C Sharp file Added

---
+++ TypeCasting/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TypeCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type Casting");
+            int a = 51;
+            float f = a;
+            Console.WriteLine("Int to float data type "+f);
+            Console.WriteLine("Enter your name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter 1st number");
+            float d = float.Parse(Console.ReadLine());
+
+            //************Concat**************
+            string c = "50";
+            string b = "25";
+            Console.WriteLine(c+b);
+            Console.WriteLine("Hello" + "" + "String" + "!");
+
+            //*****Interpolation*********
+            string author = "Ajay";
+            string book = "C# pogramming";
+            string bookAuthor = $"{author} is the author of book {book}";
+            Console.WriteLine(bookAuthor);
+
+            //*****InBuilt Concat**********
+            string fistName = "ajay";
+            string LastName = "Rathod";
+            Console.WriteLine(string.Concat(fistName,LastName));
+
+        }
+    }
+}
