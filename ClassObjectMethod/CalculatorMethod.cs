48cf9e41b13a1ef56a44e3e068a4ea799ace2ecd
Aj007ar <aj007.ar@gmail.com>
2022-08-03 10:38:33 +0530

    dotNet files Added

---
+++ ClassObjectMethod/CalculatorMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectMethod
+{
+  
+    internal class CalculatorMethod
+    {
+        int num1 = 10;
+        int num2 = 10;
+
+        public void Addition()
+        {
+            int result = num1 + num2;
+            Console.WriteLine(result);
+        }
+        public void substraction()
+        {
+            int result = num1 - num2;
+            Console.WriteLine(result);
+        }
+        public void multiplication()
+        {
+            int result = num1 * num2;
+            Console.WriteLine(result);
+        }
+        public void division()
+        {
+            int result = num1 / num2;
+            Console.WriteLine(result);
+        }
+    }
+}
