0823257a9b7026cf55cf1d5490517ac2f277de55
Aj007ar <aj007.ar@gmail.com>
2022-08-05 22:15:58 +0530

    Day 6 logical problem Added

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n*******************WELCOME TO LOGICAL PROGRAMS*******************");
+
+            Console.WriteLine("\n********************Fibbonacci Numbers***************************");
+            Fibbonacci fib = new Fibbonacci();
+            fib.Fibbo();
+
+            Console.WriteLine("\n\n*********************Prime Numbers*******************************");
+            Prime prm = new Prime();
+            prm.PrimeNumber();
+
+            Console.WriteLine("\n\n**********************Reversed Number****************************");
+            ReverseNumber revrs = new ReverseNumber();
+            revrs.Reverse();
+        }
+    }
+}
---
+++ LogicalPrograms/ReverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class ReverseNumber
+    {
+        public void Reverse()
+        {
+            int rev = 0;
+            Console.WriteLine("\nEnter any numbers for Reversing");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num+" original number");
+
+            while (num != 0)
+            {
+                int num2 = num % 10;
+                rev = rev * 10 + num2;
+                num /= 10;
+            }
+            Console.WriteLine(rev+" Reversed number ");
+        }
+    }
+}
