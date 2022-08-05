7684fcbde8465456e874631eed9a7f73a2e42a46
Aj007ar <aj007.ar@gmail.com>
2022-08-05 18:13:57 +0530

    Day 5 Assignment added

---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BasicPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n**********************WELCOME TO BASIC PROGRAMS*********************");
+            Console.WriteLine("\n*********************Flip Coin Program****************************");
+            FlipCoins flip = new FlipCoins();
+            flip.HaidTail();
+
+            Console.WriteLine("\n***********************Leap Year Program*****************************");
+            LeapYear leap = new LeapYear();
+            leap.Leap();
+
+            Console.WriteLine("\n***********************Power of Two***********************************");
+            PowerOfTwo pwr = new PowerOfTwo();
+            pwr.PowerTwo();
+
+            Console.WriteLine("\n***********************Prime Factors**********************************");
+            primeFactor prime = new primeFactor();
+            prime.Factor();
+
+        }
+    }
+}
---
+++ BasicPrograms/primeFactor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class primeFactor
+    {
+        public void Factor()
+        {
+            Console.WriteLine("Enter a number for prime factor");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 2; i < n; i++)
+            {
+                while (n % i == 0)
+                {
+                    Console.WriteLine(i+" ");
+                    n /= i;
+                }
+            }
+            if (n > 2)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
