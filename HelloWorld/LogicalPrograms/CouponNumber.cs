b82f8466835010a1c21616ed20d3b897adc993f1
Aj007ar <aj007.ar@gmail.com>
2022-08-11 18:10:37 +0530

    Logical File added

---
+++ HelloWorld/LogicalPrograms/CouponNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class CouponNumber
+    {
+        public void Coupons()
+        {
+            int dist=0;
+            int count = 0;
+            int newCoupon;
+            Console.WriteLine("Enter number for coupon");
+            int coupon = Convert.ToInt32(Console.ReadLine());
+            bool[] couponCollect = new bool[coupon];
+
+            while (dist < coupon)
+            {
+                Random random = new Random();
+                newCoupon = (int)(random.NextDouble() * coupon);
+                count++;
+            }
+            if (!couponCollect[newCoupon])
+            {
+                dist++;
+                couponCollect[newCoupon] = true;
+            }
+         }
+    }
+}
---
+++ HelloWorld/LogicalPrograms/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n*******************WELCOME TO LOGICAL PROGRAMS*******************");
+
+            Console.WriteLine("1.Fibbonacci Number");
+            Console.WriteLine("2.Perfect Number Number");
+            Console.WriteLine("3.Prime Number");
+            Console.WriteLine("4.Reversed Number");
+            Console.WriteLine("5.StopWatch");
+
+            Console.WriteLine("Enter number for execute a program");
+            int check = Convert.ToInt32(Console.ReadLine());
+
+            switch (check)
+            {
+                case 1:
+                    Console.WriteLine("\n********************Fibbonacci Numbers***************************");
+                    Fibbonacci fib = new Fibbonacci();
+                    fib.Fibbo();
+                    break;
+                case 2:
+                    Console.WriteLine("\n\n*********************Perfect Numbers*******************************");
+                    PerfectNumber per = new PerfectNumber();
+                    per.Perfect();
+                    break;
+                case 3:
+                    Console.WriteLine("\n\n*********************Prime Numbers*******************************");
+                    Prime prm = new Prime();
+                    prm.PrimeNumber();
+                    break;
+                case 4:
+                    Console.WriteLine("\n\n**********************Reversed Number****************************");
+                    ReverseNumber revrs = new ReverseNumber();
+                    revrs.Reverse();
+                    break;
+                case 5:
+                    Console.WriteLine("\n\n**********************StopWatch program****************************");
+                    StopWatch stop = new StopWatch();
+                    break;
+            }
+        }
+    }
+}
